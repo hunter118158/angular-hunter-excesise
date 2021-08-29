b5f22bc59c23856ec212d3fed604e34aaef37545
hunter118158 <332718604@qq.com>
2021-08-29 11:20:00 +0800

    3 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `
+    <h1>Hello {{ name }}!</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+  @Input() hunter: string;
+
+  ngOnInit() {
+    console.log(this.hunter);
+  }
 }
